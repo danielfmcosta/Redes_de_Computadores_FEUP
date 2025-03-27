083ea0383e31bd8434c12b7d092dd8a5f6df8dc7
GonçaloSoares <up202207437@g.uporto.pt>
2025-03-27 00:27:11 +0000

    Update link_layer.c

--- TP1/src/link_layer.c
+++ TP1/src/link_layer.c
@@ -22,16 +22,18 @@ int global_fd;
 #define FALSE 0
 #define TRUE 1
 
+#define sleep_time 1
+
+// Buffer sizes
 #define BUF_SIZE 512
 #define MAX_BUF_SIZE BUF_SIZE*2
+
+// Frame constants
 #define FLAG 0x7E
 #define ESCAPE 0x7D
-
 #define A 0x03
-
 #define C_0 0x00
 #define C_1 0x40
-
 
 //SET constants
 #define BUF_SIZE_SET 5
@@ -77,45 +79,40 @@ volatile int WRITE = FALSE;
 int alarmEnabled = FALSE;
 int alarmCount = 0;
 
-
-
 int Ns = 0;
-int Nr = 1;
 
-#define sleep_time 1
-
-// calculate the size of the frame if it ends in a FLAG
+// Calcula o tamanho do frame, se e só se a frame acaber com uma FLAG
 int get_frame_length(unsigned char *frame) {
-    // find the first and last occurrence of 0x7e
-    int first_flag_index = 0;
-    int last_flag_index = 0;
+    int first_FLAG= 0;
+    int last_FLAG = 0;
     
-    // find first flag
+    // Encontra a primeira FLAG
     for (int i = 0; i < MAX_BUF_SIZE; i++) {
         if (frame[i] == FLAG) {
-            first_flag_index = i;
+            first_FLAG = i;
             break;
         }
     }
     
-    // find last flag
-    for (int i = MAX_BUF_SIZE - 1; i > first_flag_index; i--) {
+    // Encontra a ultima FLAG
+    for (int i = MAX_BUF_SIZE - 1; i > first_FLAG; i--) {
         if (frame[i] == FLAG) {
-            last_flag_index = i;
+            last_FLAG = i;
             break;
         }
     }
-    
-    // calculate the size between the first and last flags
-    return last_flag_index - first_flag_index + 1;
+
+    return last_FLAG - first_FLAG + 1;
 }
 
+// Escreve um frame
 int write_frame(unsigned char *frame) {
-    int frameSize = get_frame_length(frame);
-    int written = write(global_fd, frame, frameSize);
+    int frame_Size = get_frame_length(frame);
+    int written = write(global_fd, frame, frame_Size);
     return written;
 }
 
+// Imprime um array
 void print_array(unsigned char *argv, int size) 
 {   
     for (int i = 0; i < size; i++) {
@@ -124,6 +121,7 @@ void print_array(unsigned char *argv, int size)
     printf("\n");
 }
 
+// Função que trata do sinal de alarme
 void alarmHandler(int signal)
 {
     if (ESTABLISHMENT == FALSE) {
@@ -132,6 +130,7 @@ void alarmHandler(int signal)
     }
 }
 
+// Função que calcula o BCC2
 int get_BCC2(const unsigned char *argv, int size){
     int BCC2 = argv[0];
     for (int i = 1; i < size; i++){
@@ -140,9 +139,10 @@ int get_BCC2(const unsigned char *argv, int size){
     return BCC2;
 }
 
+// Função de byte stuffing
 unsigned char* byte_stuffing(unsigned char *frame, int inputLength) {
     static unsigned char stuffed[MAX_BUF_SIZE];
-    memset(stuffed, 0, MAX_BUF_SIZE); // Clear the buffer
+    memset(stuffed, 0, MAX_BUF_SIZE); // Dá clear ao buffer (evitar "lixo")
     int i, j = 0;
 
     stuffed[j++] = frame[0];
@@ -154,13 +154,15 @@ unsigned char* byte_stuffing(unsigned char *frame, int inputLength) {
             stuffed[j++] = frame[i];
         }
     }
+
     stuffed[j] = frame[inputLength - 1];
     return stuffed;
 }
 
+// Função de byte destuffing
 unsigned char* byte_destuffing(unsigned char *argv, int inputLength) {
     static unsigned char destuffed[BUF_SIZE] = {0};
-    memset(destuffed, 0, BUF_SIZE); // Clear the buffer
+    memset(destuffed, 0, BUF_SIZE); // Dá clear ao buffer (evitar "lixo")
     int i, j = 0;
 
     destuffed[j++] = argv[0];
@@ -172,56 +174,54 @@ unsigned char* byte_destuffing(unsigned char *argv, int inputLength) {
             destuffed[j++] = argv[i];
         }
     }
+
     destuffed[j] = argv[inputLength - 1];
     return destuffed;
 }
 
+// Função que envia SET
 void send_SET(int fd){
-    // Send SET
-    const unsigned char SET_FRAME[BUF_SIZE_SET] = { FLAG, A_SET, C_SET, BCC1_SET, FLAG };
+    const unsigned char SET_FRAME[BUF_SIZE_SET] = {FLAG, A_SET, C_SET, BCC1_SET, FLAG};
     write(fd, SET_FRAME, BUF_SIZE_SET);
     sleep(sleep_time);
 }
 
+// Função que envia UA
 void send_UA(int fd){
-    // Send UA
-    const unsigned char UA_FRAME[BUF_SIZE_UA] = { FLAG, A_UA, C_UA, BCC1_UA, FLAG };
+    const unsigned char UA_FRAME[BUF_SIZE_UA] = {FLAG, A_UA, C_UA, BCC1_UA, FLAG};
     write(fd, UA_FRAME, BUF_SIZE_UA);
     sleep(sleep_time);
 }
 
+// Função que envia DISC
 void send_DISC(int fd){
-    // Send DISC
-    const unsigned char DISC_FRAME[BUF_SIZE_DISC] = { FLAG, A_DISC, C_DISC, BCC1_DISC, FLAG };
+    const unsigned char DISC_FRAME[BUF_SIZE_DISC] = {FLAG, A_DISC, C_DISC, BCC1_DISC, FLAG};
     write(fd, DISC_FRAME, BUF_SIZE_DISC);
     sleep(sleep_time);
 }
 
+// Função que envia Reply (RR0, RR1, REJ0, REJ1)
 void send_reply(int fd, int reply){
     if(reply == C_RR_0){
         const unsigned char RR0_FRAME[BUF_SIZE_REPLY] = { FLAG, A, C_RR_0, BCC1_RR_0, FLAG };
         write(fd, RR0_FRAME, BUF_SIZE_REPLY);
-        printf("RR0 send!\n\n");
         sleep(sleep_time);
     }else if(reply == C_RR_1){
         const unsigned char RR1_FRAME[BUF_SIZE_REPLY] = { FLAG, A, C_RR_1, BCC1_RR_1, FLAG };
         write(fd, RR1_FRAME, BUF_SIZE_REPLY);
-        printf("RR1 send!\n\n");
         sleep(sleep_time);
     }else if(reply == C_REJ_0){
         const unsigned char REJ0_FRAME[BUF_SIZE_REPLY] = { FLAG, A, C_REJ_0, BCC1_REJ_0, FLAG };
         write(fd, REJ0_FRAME, BUF_SIZE_REPLY);
-        printf("REJ0 send!\n\n");
         sleep(sleep_time);
     }else if(reply == C_REJ_1){
         const unsigned char REJ1_FRAME[BUF_SIZE_REPLY] = { FLAG, A, C_REJ_1, BCC1_REJ_1, FLAG };
         write(fd, REJ1_FRAME, BUF_SIZE_REPLY);
-        printf("REJ1 send!\n\n");
         sleep(sleep_time);
     }
 }  
 
-// All read_ functions return 0 if it fails, 1 if it succeeds
+// Função que lê SET. Retorna 0 em caso de erro e 1 em caso de sucesso
 int read_SET(int fd) {
     unsigned char buf;
     int state = 0;
@@ -265,7 +265,6 @@ int read_SET(int fd) {
                 break;
             case 4: 
                 if (buf == FLAG){
-                    //printf("SET received!\n"); 
                     return 1; 
                 } else{
                     state = 0;
@@ -277,6 +276,7 @@ int read_SET(int fd) {
     return 0;
 }
 
+// Função que lê UA. Retorna 0 em caso de erro e 1 em caso de sucesso
 int read_UA(int fd){
     unsigned char buf;
     int state = 0;
@@ -320,7 +320,6 @@ int read_UA(int fd){
                 break;
             case 4: 
                 if (buf == FLAG){
-                    //printf("UA received!\n"); 
                     return 1; 
                 } else {
                     state = 0;
@@ -332,6 +331,7 @@ int read_UA(int fd){
     return 0;
 }
 
+// Função que lê DISC. Retorna 0 em caso de erro e 1 em caso de sucesso
 int read_DISC(int fd){
     unsigned char buf;
     int state = 0;
@@ -375,7 +375,6 @@ int read_DISC(int fd){
                 break;
             case 4: 
                 if (buf == FLAG){
-                    //printf("DISC received!\n"); 
                     return 1; 
                 } else {
                     state = 0;
@@ -387,12 +386,13 @@ int read_DISC(int fd){
     return 0;
 }
 
-//Returns 0 if it fails and res if it succeeds  (C_RR0,  C_RR1, C_REJ0, C_REJ1)
+// Função que lê Reply. Retorna 0 em caso de erro e (RR0, RR1, REJ0, REJ1) em caso de sucesso
 int read_Reply(int fd) {
     unsigned char buf;
     int state = 0;
     unsigned char reply[BUF_SIZE_REPLY] = {0};
     int res =0;
+
     while (state < BUF_SIZE_REPLY) {
         int bytesRead = read(fd, &buf, 1);
         if (bytesRead <= 0) return 0;
@@ -442,10 +442,10 @@ int read_Reply(int fd) {
                 break;
 
             case 3:
-                if (buf == (reply[1] ^ reply[2])) {  // BCC1 check
+                if (buf == (reply[1] ^ reply[2])) {
                     reply[state] = buf;
                     state = 4;
-                } else { //BCC1 incorrect so I must ignore with out any action
+                } else {
                     state = 0;
                     return 0;
                 }
@@ -466,9 +466,8 @@ int read_Reply(int fd) {
     return 0;
 }
 
-// NEEDS UPDATE
-//Returns 0 if it fails and the size of the frame read
-int read_I(int fd, unsigned char *frame)  {
+// Função que lê I frame. Retorna 0 em caso de erro e o tamanho do frame em caso de sucesso
+int read_I(int fd, unsigned char *frame) {
     unsigned char buf;
     int state = 0;
     int res;
@@ -484,7 +483,6 @@ int read_I(int fd, unsigned char *frame)  {
                     frame[state] = buf;
                     state = 1;
                 } else {
-                    printf("FLAG? 0x%02X\n", buf);
                     state = 0;
                     return 0;
                 }
@@ -495,7 +493,6 @@ int read_I(int fd, unsigned char *frame)  {
                     frame[state] = buf;
                     state = 2;
                 } else {
-                    printf("A? 0x%02X\n", buf);
                     state = 0;
                     return 0;
                 }
@@ -510,7 +507,6 @@ int read_I(int fd, unsigned char *frame)  {
                     frame[state] = buf;
                     state = 3;
                 } else {
-                    printf("C? 0x%02X\n", buf);
                     state = 0;
                     return 0;
                 }
@@ -521,7 +517,6 @@ int read_I(int fd, unsigned char *frame)  {
                     frame[state] = buf;
                     state = 4;
                 }  else {
-                    printf("BCC1? 0x%02X\n", buf);
                     state = 0;
                     return 0;
                 }
@@ -542,15 +537,16 @@ int read_I(int fd, unsigned char *frame)  {
         
         }
     }
-    printf("Error: Frame too long\n");
     return 0;
 }
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
-int llopen(LinkLayer connectionParameters)
-{
+// Open a connection using the "port" parameters defined in struct linkLayer.
+// Return "1" on success or "-1" on error.
+int llopen(LinkLayer connectionParameters) {
+    // Interpretar os parametros de ligação (role, baudrate, etc)
     global_fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
     if (global_fd < 0)
     {
@@ -566,7 +562,6 @@ int llopen(LinkLayer connectionParameters)
     }
     memset(&newtio, 0, sizeof(newtio));
 
-    // convert baud rate
     speed_t baud;
     switch (connectionParameters.baudRate) {
         case 9600: baud = B9600; break;
@@ -583,10 +578,8 @@ int llopen(LinkLayer connectionParameters)
     newtio.c_oflag = 0;
     newtio.c_lflag = 0;
     
-    // adjust timeout settings
     newtio.c_cc[VTIME] = connectionParameters.timeout * 10;
     newtio.c_cc[VMIN] = 0;
-
 
     tcflush(global_fd, TCIOFLUSH);
     if (tcsetattr(global_fd, TCSANOW, &newtio) == -1)
@@ -595,55 +588,54 @@ int llopen(LinkLayer connectionParameters)
         return -1;
     }
 
-       
+    printf("\n 🐧 \n");
+
     if (connectionParameters.role == LlTx) {
-        // TRANSMITTER
+        // Transmissor:
         (void)signal(SIGALRM, alarmHandler);
 
+        // Envia SET
         send_SET(global_fd);
-        printf("SET sent!\n");
 
+        // Lê UA
         if (read_UA(global_fd) == 1) {
             ESTABLISHMENT = TRUE;
-            printf("UA received, connection established!\n");
             return 1;
         }
 
+        // Se não recebe UA, reenvia SET, 3 vezes (N_TRIES)
         alarmCount = 0;
         while (alarmCount < connectionParameters.nRetransmissions && ESTABLISHMENT == FALSE) {
             if (alarmEnabled == FALSE) {       
                 send_SET(global_fd);
-                printf("Timeout occured, will resend SET!\n");
                 alarm(connectionParameters.timeout);  
                 alarmEnabled = TRUE;
-                //sleep(sleep_time); 
 
                 if (read_UA(global_fd) == 1) {
                     ESTABLISHMENT = TRUE;
                     alarm(0); 
-                    printf("UA received, connection established!\n");
                     return 1;
                 }
             }
         }
 
-        printf("ERROR: Connection failed after 3 retries.\n");
+        printf("ERROR Establishment!\n");
         return -1;
     }
 
     if (connectionParameters.role == LlRx) {
-        // RECEIVER:
+        // Receiver:
         while (1) {
+            // Lê SET, e envia UA
             if (read_SET(global_fd)) {
                 send_UA(global_fd);
-                printf("SET received, UA sent. Connection established.\n");
                 ESTABLISHMENT = TRUE;
                 return 1;
             }
         }
     }
 
-    return 1;
+    return -1;
 }
 
 ////////////////////////////////////////////////
@@ -651,8 +643,8 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 // Send data in buf with size bufSize.
 // Return number of chars written, or "-1" on error.
-int llwrite(const unsigned char *buf, int bufSize)
-{
+int llwrite(const unsigned char *buf, int bufSize) {
+    // Construir o frame com Dados e BCC2
     unsigned char frame[bufSize + 6];
     frame[0] = FLAG;         
     frame[1] = A;           
@@ -661,28 +653,19 @@ int llwrite(const unsigned char *buf, int bufSize)
     memcpy(&frame[4], buf, bufSize);   
     frame[4 + bufSize] = get_BCC2(buf, bufSize);
     frame[5 + bufSize] = FLAG; 
-
-    printf("\nFrame: ");
-    for (int i = 0; i < 6; i++) {
-        printf("0x%02X ", frame[i]);
-    }
-    for (int i = bufSize; i < bufSize + 6; i++) {
-        printf("0x%02X ", frame[i]);
-    }
     
+    // Fazer byte stuffing
     unsigned char *stuffed_buf = byte_stuffing(frame, bufSize + 6);
-    int stuffed_size = get_frame_length(stuffed_buf);
-    printf("Stuffed Frame length: %d\n", stuffed_size);
+    
+    // Enviar o frame stuffed e caso necessário reenviar
     int retries = 0;
     int written;
     (void)signal(SIGALRM, alarmHandler);
 
     while (retries < 3) {
-        printf("[llwrite] Sending stuffed frame: ");
-        //print_array(stuffed_buf);
         written = write_frame(stuffed_buf); 
         if (written == -1) {
-            printf("Error: Failed to write frame\n");
+            printf("Error! Write Frames!\n");
             return -1;
         }
         
@@ -690,21 +673,19 @@ int llwrite(const unsigned char *buf, int bufSize)
         alarmEnabled = 1;
         int response = read_Reply(global_fd);
 
+        // Verificação da resposta em casos como RR0, RR1, REJ0, REJ1
         if (response == C_RR_0 || response == C_RR_1) {
-            printf("RR%d received\n", (response == C_RR_0) ? 0 : 1);
             Ns = (response == C_RR_0) ? 0 : 1;
             alarm(0);
             return written;
         } else if (response == C_REJ_0 || response == C_REJ_1) {
-            printf("REJ%d received\n", (response == C_REJ_0) ? 0 : 1);
             retries++;
         } else {
-            printf("Error: Failed to read reply or unexpected response\n");
             retries++;
         }
     }
 
-    printf("Error: Max retransmissions reached. Frame lost.\n");
+    printf("Error: Max Retransmissions!\n");
     return -1;
 }
 
@@ -714,68 +695,56 @@ int llwrite(const unsigned char *buf, int bufSize)
 // Receive data in packet.
 // Return number of chars read, or "-1" on error.
 int llread(unsigned char *packet) {
+    // Leitura do frame I
     unsigned char stuffed_frame[BUF_SIZE] = {0};
     int frame_size = read_I(global_fd, stuffed_frame);
-    printf("Frame from read_I size: %d\n", frame_size);
+
     if (frame_size == 0) {
-        printf("Error: Failed to read I frame\n");
+        printf("Error: Read I Frame!\n");
         return -1;
     }
 
+    // Destuffing do frame
     unsigned char *destuffed_frame = byte_destuffing(stuffed_frame, frame_size);
-
-    printf("\nDestuffed frame: ");
     int frame_length = get_frame_length(destuffed_frame);
-    
-    for (int i = 0; i < 6; i++) {
-        printf("0x%02X ", destuffed_frame[i]);
-    }
-    for (int i = frame_length; i < frame_length + 6; i++) {
-        printf("0x%02X ", destuffed_frame[i]);
-    }
 
-    printf("Destuffed Frame length: %d\n", frame_length);
-
-
+    // Caso o frame seja menor que 6 bytes, ou sejam: (FLAG, A, C, BCC1, BCC2, FLAG), é descartado imediatamente
     if (frame_length <= 6) {
-        // Must be at least 6 bytes: FLAG, A, C, BCC1, BCC2, FLAG.
-        printf("Error: Frame too short (%d bytes)\n", frame_length);
         return -1;
     }
 
-    // Verify BCC2:
-    // Compute BCC2 over the payload bytes. According to our frame format, payload is from index 4 to (frame_length - 3).
+    // Verificação do BCC2
     int computed_BCC2 = get_BCC2(&destuffed_frame[4], get_frame_length(destuffed_frame) - 6);
-    
-    // The received BCC2 is located at index frame_length - 2.
     unsigned char received_BCC2 = destuffed_frame[frame_length - 2];
     
     if (computed_BCC2 != received_BCC2) {
-        printf("BCC2 error: computed 0x%X, received 0x%X\n", computed_BCC2, received_BCC2);
-        // Send REJ depending on the control field.
+        // Em caso de erro, ou seja, BCC2 não é o esperado, envia REJ0 ou REJ1
         if (destuffed_frame[2] == C_0) {
-            send_reply(global_fd, C_REJ_0); // Send REJ0
+            send_reply(global_fd, C_REJ_0);
             llread(packet);
         } else if (destuffed_frame[2] == C_1) {
-            send_reply(global_fd, C_REJ_1); // Send REJ1
+            send_reply(global_fd, C_REJ_1);
             llread(packet);
         }
         return -1;
     }
     
+    // Calcula o número de caracteres lidos
     int payload_size = frame_length - 6;  
     for (int i = 0; i < payload_size; i++) {
         packet[i] = destuffed_frame[i + 4];
     }
     
+    // Caso o frame seja válido, envia RR0 ou RR1
     if (destuffed_frame[2] == C_0) {
-        send_reply(global_fd, C_RR_1); // RR1
+        send_reply(global_fd, C_RR_1);
     } else if (destuffed_frame[2] == C_1) {
-        send_reply(global_fd, C_RR_0); // RR0
+        send_reply(global_fd, C_RR_0);
     }
     
     return payload_size;
 }
+
 ////////////////////////////////////////////////
 // LLCLOSE
 ////////////////////////////////////////////////
@@ -785,34 +754,38 @@ int llread(unsigned char *packet) {
 int llclose(int showStatistics)
 {
     if(global_connectionParameters.role == LlTx){
-        // Send DISC
+        // Transmiter:
+
+        // Envio DISC
         send_DISC(global_fd);
 
-        // Read DISC
+        // Lê DISC
         if(read_DISC(global_fd)==0){
-            printf("Error DISC!\n");
             return -1;
         }
         
-        // Send UA
+        // Envia UA
         send_UA(global_fd);   
-    }else if(global_connectionParameters.role == LlRx){
-        // Read DISC
+    }
+    if(global_connectionParameters.role == LlRx){
+        // Receiver:
+
+        // Lê DISC
         if(read_DISC(global_fd)==0){
-            printf("Error DISC\n");
             return -1;
         }
-        // Send DISC
+
+        // Envia DISC
         send_DISC(global_fd);
-        // Read UA
+        
+        // Lê UA
         if(read_UA(global_fd)==0){
-            printf("Error UA\n");
             return -1;
         }
     }
 
-    printf("\nEnding link-layer protocol application\n");
-
+    // Finaliza o processo e fecha a ligação
+    printf("\n 🐧 \n");
     close(global_fd);
     return 1;
 }
